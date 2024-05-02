2748403eb028c02aa167fb078b84153d90fd3ae4
akeroroh <jah000118@naver.com>
2024-05-02 23:23:44 +0900

    52_2

--- Vue/5.2/vue_ex_4_2/src/components/MyComponent.vue
+++ Vue/5.2/vue_ex_4_2/src/components/MyComponent.vue
@@ -1,60 +1,30 @@
 <template>
-  <div>
-    <header class="d-block fixed-top bg-success-subtle p-2">
-      <div>
-        <h1 class="text-center fw-bold">My Gallery</h1>
-      </div>
-    </header>
+  <header class="sticky-top bg-success-subtle p-3">
+    <div class='text-center'>
+      <h1 class="fw-bold">My Gallery</h1>
+    </div>
+  </header>
 
-    <main class="container px-4">
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 mt-5 g-md-3 ">
-        <div class="col">
-          <div class="card m-3">
-            <img src="C:\Users\SSAFY\Desktop\vue_ex_4_2\src\assets\sample.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
+  <main class="container py-3">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 ">
+      <div class="col" v-for="index in numberOfItems" :key="index">
+        <div class="card">
+          <img src="@\assets\sample.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
           </div>
         </div>
-        <div class="col">
-          <div class="card m-3">
-            <img src="C:\Users\SSAFY\Desktop\vue_ex_4_2\src\assets\sample.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card m-3">
-            <img src="C:\Users\SSAFY\Desktop\vue_ex_4_2\src\assets\sample.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card m-3">
-            <img src="C:\Users\SSAFY\Desktop\vue_ex_4_2\src\assets\sample.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-
       </div>
-    </main>
-  </div>
+    </div>
+  </main>
 </template>
 
 <script setup>
+  import { ref } from 'vue';
+
+  const numberOfItems = ref(4);
 </script>
 
 <style scoped>
