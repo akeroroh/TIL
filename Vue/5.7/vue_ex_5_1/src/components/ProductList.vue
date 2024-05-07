d4a77058f033ea31f6156e5b54df422d0f9698d0
akeroroh <jah000118@naver.com>
2024-05-07 17:32:34 +0900

    57

---
+++ Vue/5.7/vue_ex_5_1/src/components/ProductList.vue
@@ -0,0 +1,15 @@
+<template>
+<li v-for="product in products" :key="product.id">
+    {{ product.name }} - {{ product.price }}
+</li>
+</template>
+
+<script setup>
+defineProps({
+    products: Array
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ Vue/5.7/vue_ex_5_2/src/components/ProductListItem.vue
@@ -0,0 +1,13 @@
+<template>
+{{ product.name }} - {{ product.price }}원 <button>장바구니에 추가</button>
+</template>
+
+<script setup>
+defineProps({
+    product: Array
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
