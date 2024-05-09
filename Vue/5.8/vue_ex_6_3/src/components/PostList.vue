d3e04f96873cb6c863b2e20cb5549da447c4a1bf
akeroroh <jah000118@naver.com>
2024-05-09 23:57:28 +0900

    58

---
+++ Vue/5.8/vue_ex_6_3/src/components/PostList.vue
@@ -0,0 +1,23 @@
+<template>
+<h2>이 작성한 게시글</h2>
+<ol>
+    <PostListItem v-for="post in posts" :post="post" :key="post.id"/>
+</ol>
+<hr>
+</template>
+
+<script setup>
+import PostListItem from '@/components/PostListItem.vue'
+import {ref} from 'vue'
+
+let id = 0
+const posts = ref([
+    {id:id++, title: 'Post1', content: 'Content1'},
+    {id:id++, title: 'Post2', content: 'Content2'},
+    {id:id++, title: 'Post3', content: 'Content3'},
+])
+</script>
+
+<style scoped>
+
+</style>
---
+++ Vue/5.8/vue_ex_6_3/src/components/PostListItem.vue
@@ -0,0 +1,15 @@
+<template>
+<li>
+    <p>제목: {{ post.name }}</p>
+</li>
+</template>
+
+<script setup>
+defineProps({
+    post: Object,
+})
+</script>
+
+<style scoped>
+
+</style>
