97ae28822a02f1d2606dca7bb263bd0e0384ed8f
akeroroh <jah000118@naver.com>
2024-05-13 23:18:41 +0900

    513

---
+++ Vue/5.13/vue_ex_8_1/vue-project/src/views/TodoView.vue
@@ -0,0 +1,21 @@
+<template>
+<main>
+  <h1>할 일 목록</h1>
+</main>
+<p>{{ store.todos }}</p>
+
+</template>
+
+<script setup>
+import {onMounted} from 'vue'
+import {useTodoStore} from '@/stores/todoStore'
+
+const store = useTodoStore()
+onMounted(() => {
+  store.getTodos()
+})
+</script>
+
+<style>
+
+</style>
