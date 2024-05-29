17d8566d28e6831abce7198051164adc6d248dff
akeroroh <jah000118@naver.com>
2024-05-29 23:42:55 +0900

    513_2

---
+++ Vue/5.13/vue_ex_8_2/vue-project/src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <TodoListItem
+            v-for="todo in todos"
+            :key="todo.id"
+            :todo="todo"
+        />
+    </div>
+</template>
+
+<script setup>
+import TodoListItem from "@/components/TodoListItem.vue";
+const props = defineProps({
+    todos: Object
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ Vue/5.13/vue_ex_8_2/vue-project/src/components/TodoListItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <P>{{ todo.work }}</P>
+        <P>{{ todo.content }}</P>
+        <P>{{ todo.is_completed }}</P>
+        <P>{{ todo.created_at }}</P>
+        <hr>
+    </div>
+</template>
+
+<script setup>
+import {defineProps} from 'vue'
+defineProps({
+    todo: Object
+})
+</script>
+
+<style scoped>
+
+</style>
